9b17eaddba6abbe8b65f5be85a214ff22636c30c
AlecAndrew <alecpro13@gmail.com>
2021-03-02 17:12:50 -0600

    Update productListing.js

--- src/main/resources/static/scripts/productListing.js
+++ src/main/resources/static/scripts/productListing.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const productListElements = document.getElementById("productsListing").children;
-
+	// test main menu button
+	getMainMenuActionElement().addEventListener(
+		"click",
+		() => { window.location.assign("/mainMenu"); });
+	
 	for (let i = 0; i < productListElements.length; i++) {
 		productListElements[i].addEventListener("click", productClick);
 	}
@@ -23,6 +27,10 @@ function findClickedListItemElement(clickedTarget) {
 
 		return (ancestorIsListItem ? ancestorElement : null);
 	}
+}
+
+function getViewProductsActionElement() {
+	return document.getElementById("mainMenuButton");
 }
 
 function productClick(event) {
