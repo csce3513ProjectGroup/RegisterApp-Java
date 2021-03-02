2fc29d44e710633e042f37ac4b64e5d1234a3b42
rhyava <47902514+Rhyava@users.noreply.github.com>
2021-03-02 17:07:54 -0600

    Created EmployeeSignIn.java Task 4.1

---
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -0,0 +1,33 @@
+package edu.uark.registerapp.models.api;
+
+// import org.apache.commons.lang3.StringUtils;
+
+public class EmployeeSignIn {
+    private String employeeId;
+    private String password;
+
+    public EmployeeSignIn() {
+        // VARIANCE: Might need to use StringUtils.Empty instead of ""
+        this.password = "";
+        this.employeeId = "";
+    }
+
+    // get methods
+    public String getPassword() {
+        return this.password;
+    }
+    public String getEmployeeId() {
+        return this.employeeId;
+    }
+
+    // set methods
+    public EmployeeSignIn setEmployeeId(final String employeeId) {
+        this.employeeId = employeeId;
+		return this;
+    }
+
+    public EmployeeSignIn setPassword(final String password) {
+        this.password = password;
+		return this;
+    }
+}
