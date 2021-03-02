8d6fc4421b3fa3484e811cce9a50285a325b570c
rhyava <47902514+Rhyava@users.noreply.github.com>
2021-03-02 13:40:06 -0600

    password hashing finished in EmployeeHelper.java, EmployeeSignInCommand.java added by mistake, not functional yet.

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -1,49 +1,77 @@
 package edu.uark.registerapp.commands.employees;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.uark.registerapp.commands.VoidCommandInterface;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
 
 @Service
-public class EmployeeSignInCommand implements VoidCommandInterface {
+public class EmployeeSignInCommand { // implements VoidCommandInterface {
     // Properties: EmployeeSignIn object and Current Session Key
         // where do I get session key?
+    
+    @Transactional
+    // @Override
+    public void execute(final int employeeId, final String password) {
+        // TODO: PARAMETERS NEED TO BE CHANGED TO REFLECT INCOMING VALUE AS EMPLOYEE OBJECT -KR
+        final String employeeIdAString = Integer.toString(employeeId);
+        
+        // Validate the incoming Employee request object
+            // Employee ID should not be blank
+            // TODO: Employee ID should be a number
+            // Password should not be blank
+        if (employeeIdAString.isBlank()) {
+            throw new UnprocessableEntityException("Employee ID");
+        }
+        else if (password.isBlank()) {
+            throw new UnprocessableEntityException("Password");
+        }
+        // Query the employee by the employee ID
+            // Use EmployeeRepository.queryByEmployeeID()
+        final Optional <EmployeeEntity> employee = this.employeeRepository.findByEmployeeId(employeeId);
+            
+            // verify that the employee exists
+            // verify that the password provided matches the database value
+                // use Arrays.equal() -- TODO: Access the password stored in the entity.
+        // if (!employee.isPresent() || !Arrays.equals(password, )) {
 
-    @Override
-    public void execute() {
-
+        // }
+        // Query the activeuser table for a record with the given employee ID
+            // EmployeeRepository again I think but what method?
+            // Use @Transactional annotation on method
+            // if an activeuser record already exists
+                // Update the record's sessionKey property with the provided session key
+                // Use the existing ActiveUserRepository.save() method
+                    // ActiveUserRepository extends the CrudRepository which provides the .save() method
+            // else {
+                // create an activeuser record in the database
+                // set the session key
+                // set the necessary employee details
+                    // which ones are necessary? - What fields are required by the database? 
+                        // ActiveUserEntity.java: id, classification, name, employeeId, sessionKey
+                // use the existing ActiveUserRepository.save() method
+            //}
     }
 
-    // Validate the incoming Employee request object
-        // Employee ID should not be blank
-        // Employee ID should be a number
-        // Pasword should not be blank
-    
-    // Query the employee by the employee ID
-        // Use EmployeeRepository.queryByEmployeeID()
-        // verify that the employee exists
-        // verify that the password provided matches the database value
-            // use Arrays.equal()
-    
-    // Query the activeuser table for a record with the given employee ID
-        // EmployeeRepository again I think but what method?
-        // Use @Transactional annotation on method
-        // if an activeuser record already exists
-            // Update the record's sessionKey property with the provided session key
-            // Use the existing ActiveUserRepository.save() method
-                // ActiveUserRepository extends the CrudRepository which provides the .save() method
-        // else {
-            // create an activeuser record in the database
-            // set the session key
-            // set the necessary employee details
-                // which ones are necessary? - What fields are required by the database? 
-                    // ActiveUserEntity.java: id, classification, name, employeeId, sessionKey
-            // use the existing ActiveUserRepository.save() method
-        //}
+    // Properties (Based on Product Codes)
+	private String sessionKey;
 
+	public String getSessionKey() {
+		return this.sessionKey;
+	}
+
+	public EmployeeSignInCommand setSessionKey(final String sessionKey) {
+		this.sessionKey = sessionKey;
+		return this;
+	}
 
 // Create Repository Class Instances
     // create an instance of the class to be used when running the methods
--- src/main/java/edu/uark/registerapp/commands/employees/helpers/EmployeeHelper.java
+++ src/main/java/edu/uark/registerapp/commands/employees/helpers/EmployeeHelper.java
@@ -2,6 +2,9 @@ package edu.uark.registerapp.commands.employees.helpers;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class EmployeeHelper {
 	public static String padEmployeeId(final int employeeId) {
 		final String employeeIdAsString = Integer.toString(employeeId);
@@ -15,8 +18,19 @@ public class EmployeeHelper {
 	}
 
 	public static byte[] hashPassword(final String password) {
-		// TODO: Hash the password using a MessageDigest. An example can be found at http://tutorials.jenkov.com/java-cryptography/messagedigest.html
-		return new byte[0];
+		// Hash the password using a MessageDigest. An example can be found at http://tutorials.jenkov.com/java-cryptography/messagedigest.html
+		try {
+			final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+			// I don't think it's necessary to do messageDigest.update(password.getBytes());
+			// and return messageDigest.digest(); since 
+				// 1) there's only one piece of data
+				// 2) the .digest() method calls the update() method internally
+			// but I could be wrong - if this doesn't work use functional code. -KR
+			return messageDigest.digest(password.getBytes());
+		}
+		catch (NoSuchAlgorithmException e){
+			return new byte[0];
+		}
 	}
 
 	private static final int EMPLOYEE_ID_MAXIMUM_LENGTH = 5;
