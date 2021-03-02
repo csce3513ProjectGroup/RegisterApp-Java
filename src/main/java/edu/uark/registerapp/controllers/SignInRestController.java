5baf34f8bb7b86b8d5b05f1261e0b8a96d0e6115
ahw0010 <77364083+ahw0010@users.noreply.github.com>
2021-03-02 17:57:40 -0600

    Update SignInRestController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
@@ -20,7 +20,7 @@ public class SignInRestController extends BaseRestController {
 		final HttpServletRequest request
 	) {
 
-		this.deleteUserCommand
+		this.activeUserDeleteCommand
 			.setSessionKey(request.getSession().getId())
 			.execute();
 
@@ -28,5 +28,5 @@ public class SignInRestController extends BaseRestController {
 			.setRedirectUrl(ViewNames.SIGN_IN.getRoute());
 	}
 	@Autowired
-	private DeleteUserCommand deleteUserCommand;
+	private ActiveUserDeleteCommand activeUserDeleteCommand;
 }
