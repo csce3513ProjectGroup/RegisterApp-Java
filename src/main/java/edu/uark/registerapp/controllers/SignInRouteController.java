62876f9304736e4ec854a810a4ff68c2d80ae2ed
Gbmeaper77 <77367006+Gbmeaper77@users.noreply.github.com>
2021-03-02 17:12:51 -0600

    Update SignInRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -8,25 +8,87 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.employees.EmployeeSignInCommand;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.controllers.enums.QueryParameterNames;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/")
 public class SignInRouteController extends BaseRouteController {
-	// TODO: Route for initial page load
+	//Route for initial page load
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showSignIn(@RequestParma final Map<String, String> queryParameters)
+	{
+		try {
+			this.activeEmployeeExistsQuery.execute();
+		} catch (NotFoundException e) {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.EMPLOYEE_DETAIL.getRoute()));
+		}
+
+		ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.SIGN_IN.getViewName()),
+				queryParameters);
+		
+		if (queryParameters.containsKey(QueryParameterNames.EMPLOYEE_ID.getValue())) {
+			modelAndView.addObject(
+				ViewModelNames.EMPLOYEE_ID.getValue(),
+				queryParameters.get(QueryParameterNames.EMPLOYEE_ID.getValue()));
+		}
+
+		return modelAndView;
+	}
+
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
+		EmployeesSignIn employeeSignIn,
 		HttpServletRequest request
 	) {
 
-		// TODO: Use the credentials provided in the request body
-		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
-		//  to sign in the user
+
+		// sign in the user
+		try {
+			this.employeeSignInCommand
+				.setSessionId(request.getSession().getId())
+				.setEmployeeSignIn(employeeSignIn)
+				.execute();
+		} catch (Exception e) {
+			ModelAndView modelAndView =
+				new ModelAndView(ViewNames.SIGN_IN.getViewName());
+
+			modelAndView.addObject(
+				ViewModelNames.ERROR_MESSAGE.getValue(),
+				e.getMessage());
+			modelAndView.addObject(
+				ViewModelNames.EMPLOYEE_ID.getValue(),
+				employeeSignIn.getEmployeeId());
+
+			return modelAndView;
+		}
 
 		return new ModelAndView(
 			REDIRECT_PREPEND.concat(
 				ViewNames.MAIN_MENU.getRoute()));
 	}
+
+		return new ModelAndView(
+			REDIRECT_PREPEND.concat(
+				ViewNames.MAIN_MENU.getRoute()));
+	}
+	//Properties
+	@Autowired
+	private EmployeeSignInCommand employeeSignInCommand;
+
+	@Autowired
+	private ActiveEmployeeExistsQuery activeEmployeeExistsQuery;
 }
