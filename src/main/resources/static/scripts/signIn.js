a0863f2987da461af6a985816130a856ad6682d8
creighton1199 <77364452+creighton1199@users.noreply.github.com>
2021-03-03 19:49:03 -0600

    Update signIn.js

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -9,6 +9,7 @@ function validateForm() {
 	if (employeeId != '' && password != ''){
 		if (isNaN(employeeId)) {
 			document.getElementById("errorMessage").innerHTML = "You can use only index"
+			alert("You can use only index");
 		}
 		else {
 			//TODO: invoke sign in
@@ -18,6 +19,7 @@ function validateForm() {
 	}
 	else{
 		document.getElementById("errorMessage").innerHTML = "Please do not keep blank Employee ID & Password"
+		alert("Please do not keep blank Employee ID & Password");
 		return false;
 	}
 }
