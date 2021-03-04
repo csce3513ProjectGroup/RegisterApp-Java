d33a1d131a47e034daf8995665e8a39fbe371e45
rhyava <47902514+Rhyava@users.noreply.github.com>
2021-03-04 09:13:23 -0600

    Defined main page, commented out active user check

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -21,20 +21,21 @@ import edu.uark.registerapp.models.api.EmployeeSignIn;
 import java.util.Map;
 
 @Controller
-@RequestMapping(value = "/signIn")
+@RequestMapping(value = "/") //"/signIn"
 public class SignInRouteController extends BaseRouteController {
 	//Route for initial page load
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView showSignIn(@RequestParam final Map<String, String> queryParameters)
 		
 	{
-		try {
-			this.activeEmployeeExistsQuery.execute();
-		} catch (NotFoundException e) {
-			return new ModelAndView(
-				REDIRECT_PREPEND.concat(
-					ViewNames.EMPLOYEE_DETAIL.getRoute()));
-		}
+		// if signed in go to main menu, if no active users go to signIn
+		// try {
+		// 	this.activeEmployeeExistsQuery.execute();
+		// } catch (NotFoundException e) {
+		// 	return new ModelAndView(
+		// 		REDIRECT_PREPEND.concat(
+		// 			ViewNames.EMPLOYEE_DETAIL.getRoute()));
+		// }
 
 		ModelAndView modelAndView =
 			this.setErrorMessageFromQueryString(
@@ -51,39 +52,39 @@ public class SignInRouteController extends BaseRouteController {
 	}
 
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ModelAndView performSignIn(
-		// TODO: Define an object that will represent the sign in request and add it as a parameter here
-		EmployeeSignIn employeeSignIn,
-		HttpServletRequest request
-	) {
+	// @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	// public ModelAndView performSignIn(
+	// 	// TODO: Define an object that will represent the sign in request and add it as a parameter here
+	// 	EmployeeSignIn employeeSignIn,
+	// 	HttpServletRequest request
+	// ) {
 
 
-		// sign in the user
-		try {
-			this.employeeSignInCommand
-				.setSessionId(request.getSession().getId())
-				.setEmployeeSignIn(employeeSignIn)
-				.execute();
-		} catch (Exception e) {
-			ModelAndView modelAndView =
-				new ModelAndView(ViewNames.SIGN_IN.getViewName());
+	// 	// sign in the user
+	// 	try {
+	// 		this.employeeSignInCommand
+	// 			.setSessionId(request.getSession().getId())
+	// 			.setEmployeeSignIn(employeeSignIn)
+	// 			.execute();
+	// 	} catch (Exception e) {
+	// 		ModelAndView modelAndView =
+	// 			new ModelAndView(ViewNames.SIGN_IN.getViewName());
 
-			modelAndView.addObject(
-				ViewModelNames.ERROR_MESSAGE.getValue(),
-				e.getMessage());
-			modelAndView.addObject(
-				ViewModelNames.EMPLOYEE_ID.getValue(),
-				employeeSignIn.getEmployeeId());
+	// 		modelAndView.addObject(
+	// 			ViewModelNames.ERROR_MESSAGE.getValue(),
+	// 			e.getMessage());
+	// 		modelAndView.addObject(
+	// 			ViewModelNames.EMPLOYEE_ID.getValue(),
+	// 			employeeSignIn.getEmployeeId());
 
-			return modelAndView;
-		}
+	// 		return modelAndView;
+	// 	}
 
-		return new ModelAndView(
-			REDIRECT_PREPEND.concat(
-				ViewNames.MAIN_MENU.getRoute()));
+	// 	return new ModelAndView(
+	// 		REDIRECT_PREPEND.concat(
+	// 			ViewNames.MAIN_MENU.getRoute()));
 	
-	}
+	// }
 	//Properties
 	@Autowired
 	private EmployeeSignInCommand employeeSignInCommand;
