87d0694f62afda547a21296a7de71041ba19b1c5
rhyava <47902514+Rhyava@users.noreply.github.com>
2021-03-04 09:11:47 -0600

    Commented out check for active user

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -26,9 +26,9 @@ public class MainMenuRouteController extends BaseRouteController {
 
 		final Optional<ActiveUserEntity> activeUserEntity =
 			this.getCurrentUser(request);
-		if (!activeUserEntity.isPresent()) {
-			return this.buildInvalidSessionResponse();
-		}
+		// if (!activeUserEntity.isPresent()) {
+		// 	return this.buildInvalidSessionResponse();
+		// }
 		
 		ModelAndView modelAndView =
 			this.setErrorMessageFromQueryString(
