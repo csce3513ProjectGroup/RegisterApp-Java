353340f61de439c5c3f25de5fd49d86ee1098385
rhyava <47902514+Rhyava@users.noreply.github.com>
2021-03-16 16:22:54 -0500

    Created EmployeeQuery Class

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeQuery.java
@@ -0,0 +1,11 @@
+package edu.uark.registerapp.commands.employees;
+
+import org.springframework.stereotype.Service;
+
+// import edu.uark.registerapp.commands.ResultCommandInterface;
+// import edu.uark.registerapp.models.api.Employee;
+
+@Service
+public class EmployeeQuery {
+
+    }
