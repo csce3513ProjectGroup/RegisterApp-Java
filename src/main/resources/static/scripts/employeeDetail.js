05283bb6057dbae36966fc6e331f695ead7a70ae
IsogaiYugo <41026474+IsogaiYugo@users.noreply.github.com>
2021-03-17 06:46:49 +0900

    Create employeeDetail.js

---
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	// TODO: Anything you want to do when the page is loaded?
+});
+
+function validateForm() {
+  /*
+	var employeeId = document.getElementById("employeeId").value;
+	var password = document.getElementById("password").value;
+
+	if (employeeId != '' && password != ''){
+		if (isNaN(employeeId)) {
+			document.getElementById("errorMessage").innerHTML = "You can use only index"
+			alert("You can use only index");
+		}
+		else {
+			document.getElementById("errorMessage").innerHTML = "Sign in successful!!"
+			return true;
+		}
+	}
+	else{
+		document.getElementById("errorMessage").innerHTML = "Please do not keep blank Employee ID & Password"
+		alert("Please do not keep blank Employee ID & Password");
+		return false;
+	}
+  */
+}
