945e6685a72f8f4e7ff6b5c01518909dee46b628
Gbmeaper77 <77367006+Gbmeaper77@users.noreply.github.com>
2021-03-16 17:04:52 -0500

    Update EmployeeDetailRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
@@ -8,48 +8,105 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistQuery;
+import edu.uark.registerapp.commands.employees.EmployeeQuery;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
-import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.api.EmployeeType;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+
 
 @Controller
 @RequestMapping(value = "/employeeDetail")
-public class EmployeeDetailRouteController {
+public class EmployeeDetailRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start() {
-		return (new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()))
-			.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
+	public ModelAndView start(@RequestParam final Map<String, String> quearyParameters, final HttpServletRequest request) {
+		final boolean activeUserExists = this.activeUserExists();
+		
+		if(activeUserExists)
+		{
+			final Optional<ActiveUserEntity> activeUserEntity = this.getCurrentUser(request);
+			if(!activeUserEntity.isPresent())
+			{
+				return this.buildInvalidSessionResponse();
+			}
+			else if (!this.isElevatedUser(activeUserEntity.get()))
+			{
+				return this.buildNoPermissionsResponse();
+			}
+		}
+		
+		return this.buildStartResponse(!activeUserExists, queryParameters);
+	}
+		
+	@RequestMapping(value = "/{employeeId}", method = RequestMethod.GET)
+	public ModelAndView startWithEmployee(@PathVariable final UUID employeeId, @RequestParam final Map<String, String> queryParameters, final HttpServletRequest request) 
+	{
+
+		final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		} else if (!this.isElevatedUser(activeUserEntity.get())) {
+			return this.buildNoPermissionsResponse();
+		}
+
+		return this.buildStartResponse(employeeId, queryParameters);
 	}
 
-	@RequestMapping(value = "/{productId}", method = RequestMethod.GET)
-	public ModelAndView startWithProduct(@PathVariable final UUID productId) {
-		final ModelAndView modelAndView =
-			new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName());
-
+	// Helper methods
+	private boolean activeUserExists() {
 		try {
-			modelAndView.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				this.productQuery.setProductId(productId).execute());
-		} catch (final Exception e) {
-			modelAndView.addObject(
-				ViewModelNames.ERROR_MESSAGE.getValue(),
-				e.getMessage());
-			modelAndView.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product())
-					.setCount(0)
-					.setLookupCode(StringUtils.EMPTY));
+			this.activeEmployeeExistsQuery.execute();
+			return true;
+		} catch (final NotFoundException e) {
+			return false;
 		}
+	}
+
+	private ModelAndView buildStartResponse(final boolean isInitialEmployee, final Map<String, String> queryParameters)
+	{
+
+		return this.buildStartResponse(isInitialEmployee, (new UUID(0, 0)), queryParameters);
+	}
+
+	private ModelAndView buildStartResponse(final UUID employeeId,final Map<String, String> queryParameters)
+	{
+
+		return this.buildStartResponse(false, employeeId, queryParameters);
+	}
+
+	private ModelAndView buildStartResponse(final boolean isInitialEmployee, final UUID employeeId, final Map<String, String> queryParameters)
+	{
+
+		ModelAndView modelAndView = this.setErrorMessageFromQueryString(new ModelAndView(ViewNames.EMPLOYEE_DETAIL.getViewName()),queryParameters);
+
+		if (employeeId.equals(new UUID(0, 0))) {
+			modelAndView.addObject(ViewModelNames.EMPLOYEE.getValue(),(new Employee()).setIsInitialEmployee(isInitialEmployee));
+		} else {
+			try {
+				modelAndView.addObject(ViewModelNames.EMPLOYEE.getValue(), this.employeeQuery.setEmployeeId(employeeId).execute().setIsInitialEmployee(isInitialEmployee));
+			} catch (final Exception e) {
+				modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), e.getMessage());
+				modelAndView.addObject(ViewModelNames.EMPLOYEE.getValue(), (new Employee()).setIsInitialEmployee(isInitialEmployee));
+			}
+		}
+
+		modelAndView.addObject(ViewModelNames.EMPLOYEE_TYPES.getValue(), EmployeeType.allEmployeeTypes());
 
 		return modelAndView;
 	}
 
 	// Properties
 	@Autowired
-	private ProductQuery productQuery;
+	private EmployeeQuery employeeQuery;
+	
+	@Autowired
+	private ActiveEmployeeExistsQuery activeEmployeeExistsQuery;
 }
