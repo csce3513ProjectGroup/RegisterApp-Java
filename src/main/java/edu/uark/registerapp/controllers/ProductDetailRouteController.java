d7e5baba644a6f0c3626db1c62fa07fd1b224d6f
AlecAndrew <alecpro13@gmail.com>
2021-03-16 17:25:08 -0500

    Update ProductDetailRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
@@ -1,19 +1,22 @@
 package edu.uark.registerapp.controllers;
 
-import java.util.UUID;
+import java.util.Map;
+import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.commands.products.ProductsQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
 @RequestMapping(value = "/productDetail")
